3287e582bd18cbb17867450ecb0bc6036f6a044a
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-05-18 21:46:01 -0700

    added a firestore db initialization & added a setDoc function to add new users when logging in with google sign in

--- src/routes/Sign-In/sign-in.component.jsx
+++ src/routes/Sign-In/sign-in.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 export default function SignIn() {
   const logGoogleUser = async () => {
-      const response = await signInWithGooglePopup();
-      console.log(response);
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = createUserDocumentFromAuth(user)
   }
     return (
     <div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,10 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { 
-        getAuth, 
-        signInWithRedirect,
-        signInWithPopup,
-        GoogleAuthProvider
+    getAuth, 
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
     } from 'firebase/auth';
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'; 
 
 const firebaseConfig = {
     apiKey: "AIzaSyDCdLbyS2tZhskfYShzRmp63jrTS_vBwQ0",
@@ -26,3 +33,33 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+ 
+  export const createUserDocumentFromAuth = async(userAuth) => {
+      const userDocRef = doc(db, 'users', userAuth.uid)
+      console.log(userDocRef)
+
+      const userSnapshot = await getDoc(userDocRef);
+      console.log(userSnapshot);
+      console.log(userSnapshot.exists());
+
+      // if user data exists
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+    // create/set the document with the data from userAuth in my collection
+    return userDocRef;
+      // return userDocRef
+  }
