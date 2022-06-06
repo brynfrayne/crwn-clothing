1f5eb2b7edff895ed9239d796177e32ac3b0c15f
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-06-05 17:45:42 -0700

    added cart item counter

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,14 @@ import './cart-icon.styles.scss';
 
 
 export default function CartIcon() {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
-
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  console.log(cartCount);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
 
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
         <ShoppingIcon className='shopping-icon'/> 
-        <span className='item-count' >10</span>
+        <span className='item-count' >{cartCount}</span>
     </div>
   )
 }
